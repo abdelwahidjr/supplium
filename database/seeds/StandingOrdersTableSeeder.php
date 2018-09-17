ec1c3a3c488a7c8f163de4389b7619f71949a23a
Abd El-Wahid JR <abdelwahid.jr@gmail.com>
2018-09-17 18:28:11 +0200

    database minor fix

--- database/seeds/StandingOrdersTableSeeder.php
+++ database/seeds/StandingOrdersTableSeeder.php
@@ -24,23 +24,13 @@ class StandingOrdersTableSeeder extends Seeder
                 $rand_day1   = $repeat_days[array_rand($repeat_days)];
                 $rand_day2   = $repeat_days[array_rand($repeat_days)];
                 $rand_day3   = $repeat_days[array_rand($repeat_days)];
-                $days        = [];
-
-                if ( ! in_array($rand_day1, $days, true)) {
-                    $days[0] = $rand_day1;
-                }
-                if ( ! in_array($rand_day2, $days, true)) {
-                    $days[1] = $rand_day2;
-                }
-                if ( ! in_array($rand_day3, $days, true)) {
-                    $days[2] = $rand_day3;
-                }
+                $rand_day4   = $repeat_days[array_rand($repeat_days)];
 
                 StandingOrder::create([
                     'name'          => $faker->name,
-                    'start_date'    => $faker->date('Y-m-d'),
-                    'end_date'      => $faker->date('Y-m-d'),
-                    'repeated_days' => $days,
+                    'start_date'    => $faker->date('Y-m-d', $max = 'now'),
+                    'end_date'      => $faker->date('Y-m-d', $max = 'now'),
+                    'repeated_days' => [$rand_day1, $rand_day2, $rand_day3, $rand_day4],
                     'order_id'      => Order::all()->random()->id,
                 ]);
             }
