259fb6a5f6e67e5064d5f2af400b84c2fb159fb9
Abd EL-Wahid Jr <m.abdelwahid@madad.sa>
2018-09-21 05:20:13 +0200

    resources updates

--- resources/lang/ar/main.php
+++ resources/lang/ar/main.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'null_entity'      => 'نسخه غير موجوده',
-    'credentials'      => 'خطأ في البيانات',
-    'deleted'          => 'تم الحذف',
-    'edit_not_allowed' => 'لا يمكن التعديل',
+    'null_entity'        => 'نسخه غير موجوده',
+    'credentials'        => 'خطأ في البيانات',
+    'deleted'            => 'تم الحذف',
+    'edit_not_allowed'   => 'لا يمكن التعديل',
+    'products_not_found' => 'لا يوجد منتجات',
 
 ];
--- resources/lang/en/main.php
+++ resources/lang/en/main.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'null_entity'      => 'Entity Not Found',
-    'credentials'      => 'Wrong Credentials',
-    'deleted'          => 'Deleted',
-    'edit_not_allowed' => 'edit not allowed',
+    'null_entity'        => 'Entity Not Found',
+    'credentials'        => 'Wrong Credentials',
+    'deleted'            => 'Deleted',
+    'edit_not_allowed'   => 'edit not allowed',
+    'products_not_found' => 'products not found',
 
 ];
