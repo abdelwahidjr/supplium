2f78cdd10e64bb089a693e15ab5ecadd54267184
Abd EL-Wahid JR <m.abdelwahid@madad.sa>
2018-09-30 19:04:27 +0200

    brand and outlet credit commit

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\BrandRequest;
 use App\Http\Resources\ModelResource;
 use App\Models\Brand;
+use App\Models\Company;
+use App\Models\Plan;
 
 class BrandController extends Controller
 {
@@ -28,39 +30,73 @@ class BrandController extends Controller
 
     public function store(BrandRequest $request)
     {
-        $brand = new Brand;
-        $brand->fill($request->all());
-        $brand->created_by_user_id = $request->user()->id;
-        $brand->save();
 
-        return new ModelResource($brand);
+
+        $allow = false;
+
+        $company = Company::find($request->company_id);
+        if (!is_object($company)) {
+            {
+                return response([
+                    'message' => trans('main.null_entity'),
+                ], 422);
+            }
+        }
+
+        $brands = Brand::where('company_id', $request->company_id)->get();
+        $brand_max = Plan::select('plans.brand_max')->where('id', $company->plan_id)->first();
+        $numer_existing_brands = count($brands);
+        if ($brand_max->brand_max == 'unlimited') {
+            $allow = true;
+        } else {
+            $numer_of_max_brands = (int)$brand_max->brand_max;
+
+            $available = $numer_of_max_brands - $numer_existing_brands;
+
+            if ($available > 0) {
+
+                $allow = true;
+            } else {
+
+                $allow = false;
+            }
+        }
+        if ($allow) {
+            $brand = new Brand;
+            $brand->fill($request->all());
+            $brand->created_by_user_id = $request->user()->id;
+            $brand->save();
+            return new ModelResource($brand);
+        } else {
+            return response([
+                'message' => trans('main.check_your_plan'),
+            ], 200);
+        }
     }
 
 
     public function show($id)
     {
 
-        $Brand = Brand::with('company' , 'outlet' , 'supplier')->find($id);
+        $Brand = Brand::with('company', 'outlet', 'supplier')->find($id);
 
-        if ($Brand === null)
-        {
+        if ($Brand === null) {
             return response([
-                'message' => trans('main.null_entity') ,
-            ] , 422);
+                'message' => trans('main.null_entity'),
+            ], 422);
         }
 
         return new ModelResource($Brand);
     }
 
 
-    public function update(BrandRequest $request , $id)
+    public function update(BrandRequest $request, $id)
     {
         $brand = Brand::find($id);
-        if ($brand === null)
-        {
+        if ($brand === null) {
             return response([
-                'message' => trans('main.null_entity') ,
-            ] , 422);
+                'message' => trans('main.null_entity'),
+            ], 422);
         }
         $brand->update($request->all());
         $brand->updated_by_user_id = $request->user()->id;
@@ -73,18 +109,17 @@ class BrandController extends Controller
     public function destroy($id)
     {
         $brand = Brand::find($id);
-        if ($brand === null)
-        {
+        if ($brand === null) {
             return response([
-                'message' => trans('main.null_entity') ,
-            ] , 422);
+                'message' => trans('main.null_entity'),
+            ], 422);
         }
         $brand->delete();
 
         return response()->json([
-            'status'  => 'Success' ,
-            'message' => trans('main.deleted') ,
-        ] , 200);
+            'status' => 'Success',
+            'message' => trans('main.deleted'),
+        ], 200);
     }
 
 }
