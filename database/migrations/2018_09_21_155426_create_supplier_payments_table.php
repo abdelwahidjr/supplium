2c112249444e211cbc89e2c784bcb54e35cf8ce0
Abd EL-Wahid JR <m.abdelwahid@madad.sa>
2018-10-05 15:08:04 +0200

    format

--- database/migrations/2018_09_21_155426_create_supplier_payments_table.php
+++ database/migrations/2018_09_21_155426_create_supplier_payments_table.php
@@ -19,7 +19,7 @@ class CreateSupplierPaymentsTable extends Migration
             $table->enum('payment_type' , ['cash' , 'credit']);
             $table->string('credit_limit')->nullable();
             $table->string('remaining_limit')->nullable();
-            $table->enum('restrict', ['on' , 'off']);
+            $table->enum('restrict' , ['on' , 'off']);
             $table->string('credit_period')->nullable();
             $table->timestamp('period_renewal')->nullable();
             $table->string('payment_due_date')->nullable();
